866cd04c1a5a2b3d5963c005dc08a00899fcb722
guseyn <guseynism@gmail.com>
2019-04-04 22:17:20 +0400

    fix indentations

--- src/async/GitHubAuthRequestOptions.js
+++ src/async/GitHubAuthRequestOptions.js
@@ -11,13 +11,13 @@ class GitHubAuthRequestOptions extends AsyncObject {
   syncCall () {
     return (code) => {
       return {
-         hostname: 'github.com',
-         port: 443,
-         path: `/login/oauth/access_token?client_id=9740bb12713949b1c23d&client_secret=300c8427a2562a2851e4dc7dbc1e3a7b50328c8c&code=${code}`,
-         method: 'POST',
-         headers: {
+        hostname: 'github.com',
+        port: 443,
+        path: `/login/oauth/access_token?client_id=9740bb12713949b1c23d&client_secret=300c8427a2562a2851e4dc7dbc1e3a7b50328c8c&code=${code}`,
+        method: 'POST',
+        headers: {
           'Accept': 'application/json'
-         }
+        }
       }
     }
   }
