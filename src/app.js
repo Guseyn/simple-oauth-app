1ea9cd2348c61626b53d0f86112713b8b43710e3
guseyn <guseynism@gmail.com>
2019-04-12 21:14:38 +0400

    created

--- src/app.js
+++ src/app.js
@@ -2,17 +2,18 @@
 
 const { as } = require('@cuties/cutie')
 const { Backend, RestApi, ServingFilesEndpoint } = require('@cuties/rest')
+const { Created } = require('@cuties/created')
 const ConnectedMongoClient = require('./mongo/ConnectedMongoClient')
 const CustomNotFoundEndpoint = require('./endpoints/CustomNotFoundEndpoint')
 const CustomInternalServerErrorEndpoint = require('./endpoints/CustomInternalServerErrorEndpoint')
 const CustomIndexEndpoint = require('./endpoints/CustomIndexEndpoint')
-const CreatedSignInEndpoint = require('./endpoints/CreatedSignInEndpoint')
-const CreatedSignUpEndpoint = require('./endpoints/CreatedSignUpEndpoint')
-const CreatedGoogleAuthEndpoint = require('./endpoints/CreatedGoogleAuthEndpoint')
-const CreatedGitHubAuthEndpoint = require('./endpoints/CreatedGitHubAuthEndpoint')
-const CreatedUserProfileEndpoint = require('./endpoints/CreatedUserProfileEndpoint')
-const CreatedUpdateUserProfileEndpoint = require('./endpoints/CreatedUpdateUserProfileEndpoint')
-const CreatedDeleteUserProfileEndpoint = require('./endpoints/CreatedDeleteUserProfileEndpoint')
+const SignInEndpoint = require('./endpoints/SignInEndpoint')
+const SignUpEndpoint = require('./endpoints/SignUpEndpoint')
+const GoogleAuthEndpoint = require('./endpoints/GoogleAuthEndpoint')
+const GitHubAuthEndpoint = require('./endpoints/GitHubAuthEndpoint')
+const UserProfileEndpoint = require('./endpoints/UserProfileEndpoint')
+const UpdateUserProfileEndpoint = require('./endpoints/UpdateUserProfileEndpoint')
+const DeleteUserProfileEndpoint = require('./endpoints/DeleteUserProfileEndpoint')
 const notFoundEndpoint = new CustomNotFoundEndpoint(new RegExp(/\/not-found/))
 const internalServerErrorEndpoint = new CustomInternalServerErrorEndpoint(new RegExp(/^\/internal-server-error/))
 const path = require('path')
@@ -29,13 +30,13 @@ new ConnectedMongoClient('mongodb://localhost:27017').as('mongoClient').after(
     new RestApi(
       new CustomIndexEndpoint('./src/static/html/index.html', notFoundEndpoint),
       new ServingFilesEndpoint(new RegExp(/^\/(html|css|js|images)/), mapper, {}, notFoundEndpoint),
-      new CreatedSignUpEndpoint(new RegExp(/^\/signup/), 'POST', as('mongoClient')),
-      new CreatedSignInEndpoint(new RegExp(/^\/signin/), 'POST', as('mongoClient')),
-      new CreatedGoogleAuthEndpoint(new RegExp(/^\/google/), 'POST', as('mongoClient')),
-      new CreatedGitHubAuthEndpoint(new RegExp(/^\/github/), 'POST', as('mongoClient')),
-      new CreatedUserProfileEndpoint(new RegExp(/^\/profile/), 'GET', as('mongoClient')),
-      new CreatedUpdateUserProfileEndpoint(new RegExp(/^\/profile/), 'PUT', as('mongoClient')),
-      new CreatedDeleteUserProfileEndpoint(new RegExp(/^\/profile/), 'DELETE', as('mongoClient')),
+      new Created(SignUpEndpoint, new RegExp(/^\/signup/), 'POST', as('mongoClient')),
+      new Created(SignInEndpoint, new RegExp(/^\/signin/), 'POST', as('mongoClient')),
+      new Created(GoogleAuthEndpoint, new RegExp(/^\/google/), 'POST', as('mongoClient')),
+      new Created(GitHubAuthEndpoint, new RegExp(/^\/github/), 'POST', as('mongoClient')),
+      new Created(UserProfileEndpoint, new RegExp(/^\/profile/), 'GET', as('mongoClient')),
+      new Created(UpdateUserProfileEndpoint, new RegExp(/^\/profile/), 'PUT', as('mongoClient')),
+      new Created(DeleteUserProfileEndpoint, new RegExp(/^\/profile/), 'DELETE', as('mongoClient')),
       notFoundEndpoint,
       internalServerErrorEndpoint
     )
